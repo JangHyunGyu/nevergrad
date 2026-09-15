150210191dd96a2d36792b16e95f89a7c14de5af
장현규 <95130210+JangHyunGyu@users.noreply.github.com>
2026-09-15 17:37:26 +0900

    fix: add speaker overlays for causality branch scenes

---
+++ assets/js/scenario/speakers_overlays.js
@@ -0,0 +1,21 @@
+/**
+ * Speaker map overlays for new causality branch scenes.
+ */
+if (typeof SCENARIO_SPEAKERS === 'undefined') var SCENARIO_SPEAKERS = {};
+Object.assign(SCENARIO_SPEAKERS, {
+    "day1_eunsu_11_honest": "me",
+    "day1_eunsu_11_brief": "me",
+    "day1_eunsu_11_sea": "me",
+    "day2_night_ft_groupchat": "me",
+    "day2_night_ft_groupchat_2": "me",
+    "day2_night_ft_putdown": "me",
+    "day2_night_ft_putdown_2": "me",
+    "day3_morning_photo_back": "me",
+    "day3_morning_photo_look": "me",
+    "day4_lunch_nurse_ask_name": "me",
+    "day4_lunch_nurse_ask_name_2": "riin",
+    "day4_lunch_nurse_ask_yuna": "me",
+    "day4_lunch_nurse_ask_yuna_2": "riin",
+    "day4_lunch_nurse_leave_ask": "me",
+    "day4_lunch_nurse_leave_yuna": "me"
+});
